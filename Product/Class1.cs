1412241244856a36cb1a6438b4cf6acbff35fe86
aysel <aydamirovaaysel7@gmail.com>
2021-10-29 11:59:42 +0400

    Добавьте файлы проекта.

---
+++ Product/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product
+{
+    class Product
+    {
+
+        public string Name;
+        public string BrendName;
+        public int Price;
+    };
+}
+
+
+
+
+
+
---
+++ Product/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1,2
+            Product prd1 = new Product
+            {
+                Name = "Chips",
+                BrendName = "Leys",
+                Price = 2
+            };
+            Product prd2 = new Product
+            {
+                Name = "Yogurt",
+                BrendName = "Aktivia",
+                Price = 1
+            };
+            Product prd3 = new Product
+            {
+                Name = " Saqqiz",
+                BrendName = "Dirol",
+                Price = 1
+            };
+        }
+        #endregion
+    }
+}
